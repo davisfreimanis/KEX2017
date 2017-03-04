ea46e194329cee8a25f3238d8081bd165a77a495
Davis Freimanis <davisf@kth.se>
2017-03-04 14:18:38 +0100

    Fixed Jonas code

--- kex/src/makePoints.cpp
+++ kex/src/makePoints.cpp
@@ -1,45 +1,55 @@
 #include <cstdio>
 #include <random>
+#include "point.h"
 
 using namespace std;
+
+vector<Point*> manualGenPoints(int nmbOfPoints);
+vector<Point*> randomGenPoints(int nmbOfPoints);
 
 int main(){
 	int mode;
 	scanf("%d", &mode);
 	int nmbOfPoints;
 	scanf("%d", &nmbOfPoints);
+
+	vector<Point*> points;
+
 	switch(mode){
 		case 1:
 			points = manualGenPoints(nmbOfPoints);
 			break;
 		case 2:
-			points = randomGenPoints(nmbOfPoints)
+			points = randomGenPoints(nmbOfPoints);
+			break;
+		default:
 			break;
 	}
 
+	for (int i = 0; i < nmbOfPoints; ++i) {
+		printf("%lf %lf\n", points[i]->x, points[i]->y);
+	}
 }
 
-vector<Point> manualGenPoints(int nmbOfPoints){
-	vector<Point> pointVector(nmbOfPoints);
+vector<Point*> manualGenPoints(int nmbOfPoints){
+	vector<Point*> pointVector(nmbOfPoints);
 	for (int i = 0; i < nmbOfPoints; ++i)
-	{	
+	{
 		double x,y;
-		scanf("%d %d", &x, &y);
-		pointVector.push_back(new Point(x,y));
+		scanf("%lf %lf", &x, &y);
+		pointVector[i] = (new Point(x,y));
 	}
 
 	return pointVector;
 }
-vector<Point> randomGenPoints(int nmbOfPoints){
-	vector<Point> pointVector(nmbOfPoints);
+vector<Point*> randomGenPoints(int nmbOfPoints){
+	vector<Point*> pointVector(nmbOfPoints);
+	default_random_engine generator;
+	normal_distribution<double> distribution(0.0,10.0);
 	for (int i = 0; i < nmbOfPoints; ++i)
-	{	
-		default_random_engine generator;
-		normal_distribution<double> distribution(0.0,10.0);
-		pointVector.push_back(new Point(distribution(generator),distribution(generator)))
+	{
+		pointVector[i] = (new Point(distribution(generator),distribution(generator)));
 	}
 
 	return pointVector;
-
-
 }
